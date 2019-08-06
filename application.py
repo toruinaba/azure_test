7fe000221fcf4c7ae6f74b856a495ad8f560b25f
toruinaba <inaba.tooru@takenaka.co.jp>
2019-08-06 17:08:18 +0900

    develop/simple

--- application.py
+++ application.py
@@ -2,9 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def add():
-    if request.method == 'post':
-        data = request.data.decode('utf-8')
-        data = json.loads(data)
-    else:
-        return "hello world"
+def add(): return "hello world"
