d8047753c876d1cdb3bcaafebde27770219c7591
toruinaba <inaba.tooru@takenaka.co.jp>
2019-08-06 16:50:54 +0900

    develop/rm dir
    data directory remove

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ def add():
     if request.method == 'get':
         data = request.data.decode('utf-8')
         data = json.loads(data)
-        with open('./data/test.json', 'w') as f:
+        with open('test.json', 'w') as f:
         	json.dump(data, f, indent=4)
     else:
         return "hello world"
