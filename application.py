8301da1846cd472a248dbdd20f9309763ebd9159
toruinaba <inaba.tooru@takenaka.co.jp>
2019-08-06 16:45:31 +0900

    develop/test1
    add output json function

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def add():
+    if request.method == 'get':
+        data = request.data.decode('utf-8')
+        data = json.loads(data)
+        with open('./data/test.json', 'w') as f:
+        	json.dump(data, f, indent=4)
+    else:
+        return "hello world"
