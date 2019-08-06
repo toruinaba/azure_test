7e20d9c0343bcf82d388abf3f486ab383a306151
toruinaba <inaba.tooru@takenaka.co.jp>
2019-08-06 17:14:00 +0900

    deevelop/import flask, request

--- application.py
+++ application.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def add(): return "hello world"
+def add():
+    data = "hello world"
+    if request.method == 'post':
+        data = request.data.decode('utf-8')
+        data = json.loads(data)
+        print(data)
+        with open('test.json', 'w') as f:
+        	json.dump(data, f, indent=4)
+    else:
+        return str(data)
